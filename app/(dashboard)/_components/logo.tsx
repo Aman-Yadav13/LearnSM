9052403557492e92c5007cebd72c15b3226cd9d2
Aman-Yadav13 <amanrrzyadav@gmail.com>
2024-03-08 00:50:56 +0530

    metadata for deployment

--- app/(dashboard)/_components/logo.tsx
+++ app/(dashboard)/_components/logo.tsx
@@ -1,12 +1,15 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export const Logo = () => {
   return (
-    <div className="flex w-full flex-row items-center gap-2">
-      <Image height={30} width={30} alt="logo" src="/logo.svg" />
-      <p className="text-xl text-emerald-600">
-        Learn<span className="font-semibold">SM</span>
-      </p>
+    <div className="w-fit">
+      <Link href="/" className="flex flex-row items-center gap-x-2">
+        <Image height={30} width={30} alt="logo" src="/logo.svg" />
+        <p className="text-xl text-emerald-600">
+          Learn<span className="font-semibold">SM</span>
+        </p>
+      </Link>
     </div>
   );
 };
